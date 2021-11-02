6f84003ddcd55653d0fad6f24b7bdc159e828115
Marco Pisco <neverland@neverlandyt.com>
2021-11-02 17:14:06 +0000

    Finished JS App

--- app.cjs
+++ app.cjs
@@ -6,6 +6,35 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+  res.setHeader('Content-Type', 'text/html');
 });
 
+app.get("/sshkey", (req, res) => {
+  res.sendFile(__dirname + "/public/sshkey");
+  res.setHeader('Content-Type', 'text/plain');
+})
+app.get("/key", (req, res) => {
+  res.sendFile(__dirname + "/public/sshkey");
+  res.setHeader('Content-Type', 'text/plain');
+})
+
+app.get("/resume", (req, res) => {
+  res.sendFile(__dirname + "/public/Resume.pdf");
+  res.setHeader('Content-Type', 'application/pdf');
+})
+
+app.get("/websites", (req, res) => {
+  res.sendFile(__dirname + "/public/otherwebsites.html");
+  res.setHeader('Content-Type', 'text/html');
+})
+
+// app.get("/resume-html", (req, res) => {
+//   res.sendFile(__dirname + "/public/resume.html");
+//   res.setHeader('Content-Type', 'text/html');
+// })
+
+// app.get("/certificate", (req, res) => {
+//   res.sendFile(__dirname + "/public/certificate");
+//   res.setHeader('Content-Type', 'text/plain');
+// })
