6dffe071400f495e7ba2ae73bc2b23bfa2df1282
Marco Pisco <neverland@neverlandyt.com>
2021-11-02 17:27:39 +0000

    Updated it - once again.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require("express");
+import express from 'express';
+import path from 'path';
 const app = express();
+
+const __dirname = path.resolve(path.dirname(''));
 
 app.listen(3000, () => {
   console.log("Server is listening at port 3000.");
